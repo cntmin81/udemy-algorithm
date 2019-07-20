367e65faad0a58341cfa7f0b714f379cb329e931
YOUNGMIN SHIN <youngminshin@YOUNGMINui-MacBookPro.local>
2019-07-20 11:11:09 +0900

    Change some code

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,23 +1,19 @@
 function isPalindrome(string) {
-    // make string to lowercase
     string = string.toLowerCase();
-    var charactersArr = string.split('');
+    var characterArr = string.split('');
     var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
     var letterArr = [];
-    charactersArr.forEach(char => {
-        if (validCharacters.indexOf(char) > -1) {
+    characterArr.forEach(char => {
+        if(validCharacters.indexOf(char) > -1) {
             letterArr.push(char);
         }
     });
 
-    console.log(letterArr);
-
-    if( letterArr.join('') === letterArr.reverse().join('')) {
+    if (letterArr.join('') === letterArr.reverse().join('')) {
         return true;
     }
     return false;
-
 }
 
-console.log(isPalindrome("Madam I'm adam"));
+console.log(isPalindrome("Madam I'm adam."));
