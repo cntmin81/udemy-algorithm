db2221408c7d30aed6c701aad222084a2146f05f
YOUNGMIN SHIN <youngminshin@YOUNGMINui-MacBookPro.local>
2019-07-21 08:11:47 +0900

    Commit twoSum

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -3,7 +3,7 @@ function twoSumOSquareN(numArray, sum) {
     numArray.forEach(num => {
         var tempArr = [];
         for (var i = 0; i < numArray.length; i++) {
-            if ( num + numArray[i] === sum) {
+            if (num + numArray[i] === sum) {
                 tempArr = [num, numArray[i]];
             }
         }
@@ -15,12 +15,12 @@ function twoSumOSquareN(numArray, sum) {
     return result;
 }
 
-function twoSum(numArray, sum) {
+function myTwoSum(numArray, sum) {
     var result = [];
     var xArr = [];
     numArray.forEach(num => {
         var tempX = sum - num;
-        if(numArray.includes(tempX)) {
+        if (numArray.includes(tempX)) {
             result.push([num, tempX]);
         }
     });
@@ -28,4 +28,20 @@ function twoSum(numArray, sum) {
     return result;
 }
 
-console.log(twoSum([1,2,3,6], 9));
+function twoSum(numArray, sum) {
+    var pairs = [];
+    var hashTable = [];
+
+    for (var i = 0; i < numArray.length; i++) {
+        var currNum = numArray[i];
+        var counterPart = sum - currNum;
+        if(hashTable.indexOf(counterPart) !== -1) {
+            pairs.push([currNum, counterPart]);
+        }
+        hashTable.push(currNum);
+    }
+
+    return pairs;
+}
+
+console.log(twoSum([7,3,2,1,6], 9));
