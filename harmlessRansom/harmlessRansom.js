252a64d9e310fdb48eeb835e12de240dbe5a595d
YOUNGMIN SHIN <youngminshin@YOUNGMINui-MacBookPro.local>
2019-07-20 10:27:15 +0900

    Commit harmless Ransom

---
+++ harmlessRansom/harmlessRansom.js
@@ -0,0 +1,30 @@
+function harmlessRansome(noteText, magazineText) {
+    var noteArr = noteText.split(' ');
+    var magazineArr = magazineText.split(' ');
+    var magazineObj = {};
+
+    magazineArr.forEach(word => {
+        if (!magazineObj[word]) {
+            magazineObj[word] = 0;
+        }
+        magazineObj[word]++;        
+    });
+
+    console.log(magazineObj);
+
+    var noteIsPossible = true;
+    noteArr.forEach(word => {
+        if (magazineObj[word]) {
+            magazineObj[word]--;
+            if (magazineObj[word] < 0) {
+                noteIsPossible = false;
+            }
+        } else {
+            noteIsPossible = false;
+        }
+    });
+
+    return noteIsPossible;
+}
+
+console.log(harmlessRansome('This This', 'This is all text of magazine text'));
---
+++ harmlessRansom/note.md
@@ -0,0 +1,9 @@
+# Big O notation
+![Big O notation](https://nedbatchelder.com/pix/graph_complexity.png)
+## O(1)
+## O(n)
+Linear runtime
+## O (N^2)
+Exponential runtime
+## O (log n)
+Logarithmic runtime
